56dab41e2e3f82f6f04493ba1fcf52d49e6a9e6f
Julia Evans <julia@jvns.ca>
2026-07-27 19:06:50 -0400

    serve static files from static/

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
+
+//go:embed static
+var StaticDir embed.FS
 
 func main() {
 	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// DistDirFS is from embed.go
+		staticFS, _ := fs.Sub(StaticDir, "static")
+		se.Router.GET("/{path...}", apis.Static(staticFS, false))
+
+		return se.Next()
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
