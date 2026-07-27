df41b2334652154e0f08b164983a4d204785e348
Julia Evans <julia@jvns.ca>
2026-07-27 14:50:29 -0400

    get recogito getting started working

---
+++ script.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "bnana"
+        }
+    },
+    methods: {
+        testMethod() {
+            return this.message + "!";
+        }
+    }
+})
+app.mount('#app')
+
+window.process = { browser: true, env: { ENVIRONMENT: 'BROWSER' } };
+
+const { createTextAnnotator } = RecogitoJS;
+const anno = createTextAnnotator(document.getElementById('asdf'));
+anno.on('createAnnotation', annotation => {
+  console.log('new annotation', annotation);
+});     
