c69c70f0408061a0ea1302ace7fd6f24939b0ab7
Julia Evans <julia@jvns.ca>
2026-07-28 08:27:51 -0400

    query pocketbase api for the page

--- static/script.js
+++ static/script.js
@@ -1,21 +1,32 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            message: "bnana"
-        }
-    },
-    methods: {
-        testMethod() {
-            return this.message + "!";
-        }
-    }
-})
-app.mount('#app')
-
-window.process = { browser: true, env: { ENVIRONMENT: 'BROWSER' } };
-
+const pb = new PocketBase("http://127.0.0.1:8090");
+window.process = { browser: true, env: { ENVIRONMENT: "BROWSER" } };
 const { createTextAnnotator } = RecogitoJS;
-const anno = createTextAnnotator(document.getElementById('asdf'), {'user': {'id': 'asdf', 'name': 'Julia'}});
-anno.on('createAnnotation', annotation => {
-  console.log('new annotation', annotation);
-});     
+
+const app = Vue.createApp({
+  data() {
+    return {
+      content: "",
+    };
+  },
+
+  async mounted() {
+    const record = await pb.collection("documents").getFirstListItem(
+      'name="git-pull"',
+    );
+    this.content = record.content;
+    // this.$refs.html;
+  },
+  methods: {
+    testMethod() {
+      return this.message + "!";
+    },
+  },
+});
+app.mount("#app");
+
+const anno = createTextAnnotator(document.getElementById("asdf"), {
+  "user": { "id": "asdf", "name": "Julia" },
+});
+anno.on("createAnnotation", (annotation) => {
+  console.log("new annotation", annotation);
+});
