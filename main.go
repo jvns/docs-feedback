65f333bfe3a1ad83beb2165840b1cb6110d5a2c5
Julia Evans <julia@jvns.ca>
2026-07-28 08:12:34 -0400

    remove bad comments

--- main.go
+++ main.go
@@ -19,18 +19,17 @@ func main() {
 	app := pocketbase.New()
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-		// DistDirFS is from embed.go
 		staticFS, _ := fs.Sub(StaticDir, "static")
 		se.Router.GET("/{path...}", apis.Static(staticFS, false))
 
 		return se.Next()
 	})
 
+	// auto create migration files when making collection changes in the Dashboard
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
-		// enable auto creation of migration files when making collection changes in the Dashboard
-		// (the IsProbablyGoRun check is to enable it only during development)
 		Automigrate: osutils.IsProbablyGoRun(),
 	})
+
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
