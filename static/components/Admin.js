0e4849ae84553f3f00629cb5551df953c2a8c062
Julia Evans <julia@jvns.ca>
2026-08-04 13:56:29 -0400

    add empty admin component

---
+++ static/components/Admin.js
@@ -0,0 +1,14 @@
+import template from "./Admin.html";
+
+import { fromRecord, toRecord } from "../util.js";
+
+export default {
+  template: template,
+  data() {
+    return {};
+  },
+
+  async mounted() {},
+
+  methods: {},
+};
