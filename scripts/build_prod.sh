1ddad6ce6b5f7f3c5cfb43a80471d8bba100b087
Julia Evans <julia@jvns.ca>
2026-08-12 15:53:20 -0400

    improve css build

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,2 +1,2 @@
 esbuild static/script.js --sourcemap --bundle --alias:vue=./static/js/vue.esm-browser.prod.js --outfile=static/bundle.js --loader:.html=text
-esbuild static/style.css  --bundle --loader:.svg=dataurl --loader:.woff2=file --target=chrome87,firefox83,safari14 --outfile static/style.css --allow-overwrite
+esbuild static/style.css  --bundle --loader:.svg=dataurl --loader:.ttf=file --target=chrome87,firefox83,safari14 --outfile=static/style.css --allow-overwrite
