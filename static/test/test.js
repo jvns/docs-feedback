748ba6b1e28f7120440c68332ab918f4ef18951d
Julia Evans <julia@jvns.ca>
2026-09-10 09:13:28 -0400

    remove default value for `value` in StorageMock in tests
    there's no point

--- static/test/test.js
+++ static/test/test.js
@@ -7,7 +7,7 @@ function StorageMock() {
 
   return {
     setItem: function (key, value) {
-      storage[key] = value || "";
+      storage[key] = value;
     },
     getItem: function (key) {
       return key in storage ? storage[key] : null;
